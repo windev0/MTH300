4aa40c7d5906cdc7a80d2f60237dddc0603d2b10
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2022-11-30 23:39:43 +0000

    depot1, procedure de résolution de matrice diagonale, triangulaire inférieure, triangulaire supérieure

---
+++ equaNonLineaire.py
@@ -0,0 +1,285 @@
+from math import ceil, exp, fabs, log, log10, sqrt
+import os
+
+def nbre_solutions(x1, x2, pas):    # cette fonction retourne le nb de solution apres balayage
+    a = x1 + pas                    # initialisation de a, et servira de borne sup temporaire
+    cpt = 0                         # initialisation de cpt, pour compter les solutions
+    while a <= x2:                  # tant que nous ne sommes pas arrivé à la borne supérieure
+        x1 += pas                   # x1 est augmente d'un pas vers la gauche
+        a += pas                    # a egalement est augmente d'un pas vers la gauche
+        if f(x1)*f(a) < 0:          # si la fonction change de signe sur [x1, a], alors...
+            cpt += 1                # il y a existence d'une solution, on incrémente cpt
+    return cpt                      # on retourne le nombre de solutions trouvées
+#---------------------------------------------------------------------------------------------
+def effacer_console():
+    cmd = 'clear'
+    if os.name in ('nt', 'dos'):
+        cmd = 'cls' # cas de windows, la commande sera cls et non clear
+    os.system(cmd) # effacer la console
+#---------------------------------------------------------------------------------------------
+
+def saisie_bornes():
+    try:
+        x1 = int(input("\n\tVeuillez saisir la borne inférieure X1 = "))
+        x2 = int(input("\n\tVeuillez saisir la borne supérieure X2 = "))
+        assert x1 < x2
+        if x1 < x2:
+            return x1, x2
+    except AssertionError:
+        print('\nErreur: la borne inférieure doit etre strictement inférieure à la borne supérieure')
+    except ValueError or TypeError:
+        print("\nErreur: Veuillez bien choisir un entier !")
+#---------------------------------------------------------------------------------------------
+def saisie_X0():
+    # cette fonction permet de saisir la valeur initiale
+    try:
+        x0 = int(input("\n\tVeuillez saisir une valeur initiale: "))
+        return x0
+    except ValueError or TypeError:
+        print("\n\tErreur: Saisie invalide, choisir une autre valeur entière!")
+#---------------------------------------------------------------------------------------------
+def calcul_N(x1, x2, e):
+    # cette fonction permet de calculer le nombre maximal d'iteration avec dichotomie
+    try:
+        N = ceil((log(x2-x1) - log(2*e))/log(2))
+        print('\n\tLe nombre maximal d\'itération est N =',N)
+        return N
+    except ValueError or TypeError:
+        print("\n\tErreur: données invalide !")
+#---------------------------------------------------------------------------------------------
+def saisie_tolerance():
+    try:
+        e = float(input("\n\tQuelle est la tolérence e = "))
+        return e
+    except ValueError or TypeError:
+        print("\n\tErreur: Saisie invalide, essayer une autre valeur !")
+#---------------------------------------------------------------------------------------------
+def convergence(x1, x2, e, nb, xm = 1):
+    try:
+        if nb == 1: # 1 s'il s'agit de la convergence de la methode dichotomie
+            return (fabs(x2-x1) / 2*fabs(xm)) < e
+        else:   # autre entier pour la convergence des methodes de points fixes, NEWTON
+            return (fabs(x2-x1) / fabs(xm)) < e
+    except ZeroDivisionError:
+        print("\n\tErreur: Division par zéro, essayer une autre valeurs")
+    except TypeError or ValueError:
+        print("\n\tErreur: donnée(s) invalide(s) !")
+#---------------------------------------------------------------------------------------------
+f = lambda x : exp(-x) - x
+    # return x**3 + x**2 - 3*x - 3
+    # return pow(x,2) - 2
+    # return sqrt(2*x + 3)
+    # return pow(x,2) - 2*x - 3
+    # return pow(x,2) - 3*x + 2
+#---------------------------------------------------------------------------------------------
+devf = lambda x : -exp(-x) - 1
+    # reperesente la fonction derivee 
+#---------------------------------------------------------------------------------------------
+# permet de tronquer un nombre nb a un m chiffres apres la virgule
+def tronquer(nb, m):
+    nb = str(nb)
+    partie_ent, partie_decim = nb.split('.')
+    nb = '.'.join([partie_ent, partie_decim[:m]])
+    return float(nb) 
+
+#=============================================================================================
+
+def secante():
+    try:
+        x0 = saisie_X0()
+        x1 = saisie_X0()
+        assert x0 < x1
+        e = saisie_tolerance()
+        N = int(input("\n\tVeuillez saisir le nombre total d'itération N = "))
+        x2 = x1 - ((f(x1)*(x1-x0))/(f(x1)-f(x0)))  # initialisation de x2
+        i = 0
+        while not(convergence(x1, x2, e, 2, x2)) and i < N - 1:
+            # mise a jour des valeurs 
+            x0 = x1
+            x1 = x2
+            x2 = x1 - ((f(x1)*(x1-x0))/(f(x1)-f(x0)))
+            i += 1
+        # Saisie des résultats
+        print('\n-----------------------------------------------')
+        if convergence(x1, x2, e, 2, x2):
+            print("\tconvergence atteinte")
+        if i == N-1 and (not convergence(x1, x2, e, 2, x2)):
+            print("\tConvergence non atteinte en {} itérations".format(i+1))
+        print("\tla solution est: Xn+1 = {}".format(x2))
+        print('-----------------------------------------------')
+    except TypeError:
+        print("\n\tUne ou plusieurs donnée(s) entrée(s) est (sont) non valide(s)")
+    except AssertionError:
+        print("\n\tErreur: La premiere valeur initiale doit etre inférieure à la seconde!")
+
+#---------------------------------------------------------------------------------------------
+
+def newton():
+    try:
+        e = saisie_tolerance()
+        N = int(input("\n\tVeuillez saisir le nombre total d'itération N = "))
+        x0 = saisie_X0()
+        x1 = x0 - (f(x0) / devf(x0)) # initialisation de x1
+        i = 0
+        while not(convergence(x0, x1, e, 2, x1)) and i < N - 1:
+            # mise a jour des valeurs 
+            x0 = x1
+            x1 = x0 - (f(x0) / devf(x0))
+            i += 1
+        # Saisie des résultats
+        print('\n-----------------------------------------------')
+        if convergence(x0, x1, e, 2, x1):
+            print("\tconvergence atteinte")
+        if i == N-1 and (not convergence(x0, x1, e, 2, x1)):
+            print("\tConvergence non atteinte en {} itérations".format(i+1))
+        print("\tla solution est: Xn+1 = {}".format(x1))
+        print('-----------------------------------------------')
+    except TypeError or ValueError:
+        print("\n\tErreur: Une ou plusieurs donnée(s) entrée(s) est (sont) non valide(s)")
+    except ZeroDivisionError:
+        print("\n\tErreur: Division par zéro, essayer avec d'autres valeurs !")
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
+def point_fixe():
+    try:
+        x0 = saisie_X0()
+        x1 = f(x0)  # initialisation de x1
+        e = saisie_tolerance()
+        # N = calcul_N(x0, x1, e)
+        N = int(input("\n\tVeuillez saisir le nombre total d'itération N = "))
+        i = 0
+        while not(convergence(x0, x1, e, 2, x1)) and i < N - 1:
+            # mise a jour des valeurs 
+            x0 = x1
+            x1 = f(x0)
+            i += 1
+        # Saisie des résultats
+        print('\n-----------------------------------------------')
+        if convergence(x0, x1, e, 2, x1):
+            print("\tconvergence atteinte")
+        if i == N-1 and (not convergence(x0, x1, e, 2, x1)):
+            print("\tConvergence non atteinte en {} itérations".format(i+1))
+        print("\tla solution est: Xn+1 = {}".format(x1))
+        print('-----------------------------------------------')
+        
+    except TypeError or ValueError:
+        print("\n\tUne ou plusieurs donnée(s) entrée(s) est (sont) non valide(s)")    
+    except ZeroDivisionError:
+        print("\n\tErreur: Division par zéro, essayer avec d'autres valeurs !")
+    except OverflowError:
+        print("\n\tErreur: Dépassement de capacité !")
+#---------------------------------------------------------------------------------------------
+        
+def dichotomie():
+    try:
+        x1, x2 = saisie_bornes()    # saisie des bornes de l'intervale
+        if f(x1)*f(x2) < 0:         # la fonction change de signe sur l'intervalle donné
+            print("\n\tLa fonction est monotone sur l'intervalle [{},{}]".format(x1, x2))
+            print("\ton a {} solution(s)".format(nbre_solutions(x1, x2, 0.01))) # affichage du nombre de solutions trouvées
+            xm = (x1 + x2 )/2           # initialisation du milieu de l'intervalle
+            if f(xm) == 0:              # xm est la racine cherchee
+                print("\n\t X1 = {}\t\t\t X2 = {}\t\t\t Xm = {}\n f(X1) = {}\t\t f(X2) = {}\t\t f(Xm) = {}".format(x1, x2, xm, f(x1), f(x2), f(xm)))
+            else:
+                e = saisie_tolerance()   # saisie du critère d'arret et le nb max d'itération
+                N = calcul_N(x1, x2, e)  # calcul du nb max d'iteration a l'aide de la tolerance
+                i = 0
+                while (not (convergence(x1, x2, e, 1, xm))) and i < N-1 and f(x1)*f(x2) < 0:
+                    if f(x1)*f(xm) < 0:
+                        x2 = xm             # changement de la borne supérieure
+                    if f(xm)*f(x2) < 0:
+                        x1 = xm             # changement de la borne inférieure
+                    xm = (x1 + x2 )/2       # mise à jour du milieu de l'intervalle
+                    i += 1                  # incrémentation du nb d'itérations
+                print('\n-------------------------------------------------------------------------------------------')
+                if i == N-1 and (not convergence(x1, x2, e, 1, xm)):    # nb d'itération atteinte
+                    print("\tConvergence non atteinte en {} itérations".format(i+1))
+                if convergence(x1, x2, e, 1, xm):           # si la convergence est atteinte, alors...
+                    print("\tConvergence atteinte")
+                # Saisie des résultats
+                print("\n\t X1 = {}\t\t\t X2 = {}\t\t\t Xm = {}\n\t f(X1) = {}\t\t f(X2) = {}\t\t f(Xm) = {}".format(\
+                tronquer(x1, 6), tronquer(x2, 6), tronquer(xm, 6), tronquer(f(x1), 6), tronquer(f(x2), 6), tronquer(f(xm), 6)))
+            print('-------------------------------------------------------------------------------------------')
+        elif f(x1) == 0:
+            print('\n-------------------------------------------------------------------------------------------')
+            print("\tLa solution est x =", x1)
+            print('-------------------------------------------------------------------------------------------')
+            
+        elif f(x2) == 0:
+            print('\n-------------------------------------------------------------------------------------------')
+            print("\tLa solution est x =", x2)
+            print('-------------------------------------------------------------------------------------------')
+        else:
+            print("\n\tla fonction ne change pas de signe sur l'intervalle [{},{}]".format(x1, x2))
+
+    except TypeError or ValueError:
+        print("\n\tErreur: une ou plusieurs donnée(s) entrée(s) est (sont) non valide(s) !")
+    except ZeroDivisionError:
+        print("\n\tErreur: Division par zéro, essayer avec d'autres valeurs !")
+# -----------------------------------------------------------------------------------------------------------------------------------------
+def main():
+    effacer_console() # on rend au propre la console
+
+    # Test des différentes méthodes
+    print('\n')
+    print('\n========================== E Q U A T I O N   D U  T Y P E   F ( X ) = 0 ==========================')
+
+    a = nbre_solutions(0, 8, 0.01)
+    if a > 0:
+        print("\n Il existe {} solution (s)".format(a)) # affichage du nombre de solutions après balayage
+        
+        continuer = 'o' # variable servant de condition de continuation
+        while continuer == 'o':
+            
+            # affichage du menu
+            print("\n\t M E N U ")
+            
+            print("\n\t 1- DICHOTOMIE ")
+            print("\n\t 2- SECANTE ")
+            print("\n\t 3- NEWTON ")
+            print("\n\t 4- POINTS FIXES ")
+            try:
+                choix = int(input("\nFaire un choix entre {1} {2} {3} {4}: "))
+                if choix in [1, 2, 3, 4]:
+                    # appel aux procedures des methodes
+                    if choix == 1:
+                        print("\n\t================ D I C H O T O M I E ================")
+                        dichotomie()
+                    elif choix == 2:
+                        print("\n\t================ L A  S E C A N T E  ================")
+                        secante()
+                    elif choix == 3:
+                        print("\n\t================  N  E  W  T  O  N  ================")
+                        newton()
+                    elif choix == 4:
+                        print("\n\t================ P O I N T  F I X E ================")
+                        point_fixe()
+                    else:
+                        pass
+                else:
+                    raise PermissionError
+        
+                continuer = str.lower(input("\nVoulez vous continuer (o/n)? ")) # forcer la valeur du choix a etre en minuscule
+                if continuer != 'n' and continuer != 'o':
+                    print("\n\tErreur: Saisie invalide ")
+                
+            except PermissionError:
+                print("\nChoix invalide !")
+                
+            effacer_console() # on rend au propre la console
+
+    else:
+        print("\nAucune solution pour cette équation !")
+
+    print('\n')
+
+# main()
+# dichotomie()
+# point_fixe()
+# newton()
+# secante()
+"""print(tronquer(3.99999999999998, 5))
+liste = ['', 1, 0, 1, '\n', 4, 5, 1, "\n", 8, 1, 2]
+print(*liste)
+mot_de_passe = getpass()
+print(mot_de_passe)"""
---
+++ system.py
@@ -0,0 +1,303 @@
+import numpy as np
+from equaNonLineaire import effacer_console
+
+def matrice_diagonale(A, b):
+    try:
+        n, m, p = A.shape[0], A.shape[1], len(b)   # recueil du nb de lignes et de colonnes
+        assert n == m == p                 # verifier si la matrice est carree
+        sol = np.zeros(shape=(n, 1))
+        for i in range(n):
+            sol[i] = b[i] / A[i, i]
+        print("\n\t La solution est S = \n")
+        print(sol)    
+    except AssertionError:
+        print("\n\tERREUR: la matrice n'est pas carree ou la matrice b n'est pas un vecteur")
+        
+# ==================================================================================================
+def matrice_triangulaire_sup(A, b):
+    try:
+        n, m, p = A.shape[0], A.shape[1], len(b)   # recueil du nb de lignes et de colonnes
+        assert n == m == p                 # verifier si la matrice est carree
+        # verifier si la matrice est triangulaire sup
+        try:
+            for i in range(n):
+                assert A[i, i+1] == 0
+        except Exception:
+            print("\n\tERREUR: Matrice non triangulaire supérieure !")
+        sol = np.zeros(shape=(n, 1))    # initialisation de la matrice vecteur solution
+        sol[n-1] = b[n-1] / A[n-1, n-1]     # la solution z 
+        # la remontée
+        for i in range(n-1):
+    except AssertionError:
+        print("\n\tERREUR: la matrice n'est pas carree ou la matrice b n'est pas un vecteur")
+        
+        
+
+A = np.array([
+    [1, 0, 0],
+    [0, 5, 0],
+    [0, 0, 9]
+])
+# C = np.diag([1, 5, 9]) # pour la deckaration d'une matrice diagonale
+b = np.array([
+    [1],
+    [2],
+    [3]
+])
+matrice_diagonale(A, b)
+matrice_diagonale(C, b)
+
+
+
+def Gauss_elimination(A, B):
+    try:
+        n = A.shape[0]                  # nb de lignes
+        m = A.shape[1]                  # nb de colonnes
+        assert n == m                   # verifier si la matrice est carrée
+        Aw = np.zeros([n, n+1])         # matrice de travail, matrice augmenté
+        Aw[:, 0:n] = A                  # la matrice de travail, de 0 à m-1 colonnes contient A
+        Aw[:, n] = B                    # la derniere colonne est le vecteur B 
+        sol = np.zeros(n)               # la solution est un vecteur de meme ligne que la matrice A
+        
+        # echelonnement
+        # boucle en i(pivots) de 1 a n-1 <==> 0, 1, 2, ..., n-2 (ici n-1 a cause de arrage)
+        for i in np.arange(0, n-1):    
+            # boucle en k (lignes) de i+1 a n <==> 0, 1, 2, ..., n-1 (ici n a cause de arrage) 
+            for k in np.arange(i+1, n):
+                # formule : Aw[k, :] =  Aw[k, :] - Aw[k, i] / Aw[i, i] * Aw[i, :]
+                Aw[k, :] =  Aw[k, :] - Aw[k, i] / Aw[i, i] * Aw[i, :]
+        
+        # remontee
+        sol[n-1] = Aw[n-1, n] / Aw[n-1, n-1]    # initialisation de la solution
+        for i in np.arange(n-2, -1, -1):        # boucle de n-2 a 0 <==> n-1 a 1 en python
+            sol[i] = (Aw[i, n] - np.sum(Aw[i,i+1:n]*sol[i+1:n])) / Aw[i, i]
+            
+        print("\nX = {}".format(sol))
+        print(np.linalg.solve(A, B))
+    except AssertionError:
+        print("\nLa matice n'est pas carrée !")
+
+# ==============================================================================================
+def Gauss_jordanV2(A, B):
+    try:
+        # verifier si la matrice A est carrée
+        m, n = np.shape(A)[0], np.shape(A)[1]   # respectivemnt nb de lignes et de colonnes
+        assert m == n
+        
+        for i in range(n):
+            if A[i, i] == 0:    # le pivot est nul
+                # permutation des lignes et colonnes de A et de B
+                # on peut aussi juste remplacer la matrice par sa transposée car il s'agit d'une matrice carree
+                for k in range(n):  # on parcourt toutes les lignes
+                    while A[i, i] == 0: # pour tout pivot nul
+                        A[i, k] = A[i+1, k] # nous échageons les elément de la ligne avec ceux de la ligne suivante
+                        B[i] = B[i+1]       # nous échageons les elément de la ligne avec ceux de la ligne suivante
+                   
+            # divison par le pivot
+            B[i] = B[i] / A[i, i]
+            A[i, :] = A[i, :] / A[i, i]
+            
+            for j in range(n):  # pour chaque colonne de la ligne i
+                if j != i:  # seulement les éléments hors de la diagonale
+                    B[j] = B[j] - B[i] * A[j, i]    # nvlle valeur de bj
+                    A[j, i:n] = A[j, i:n] - (A[i, i:n] * A[j, i])
+            
+    except AssertionError: 
+        print("\nLa matrice n'est pas carrée !")
+        
+    return B, A      # B est le vecteur solution X       
+    
+# ==============================================================================================
+
+def Gauss_jordan(A, B):
+    A = np.array(A, int)
+    B = np.array(B, int)
+    n = len(B)
+    
+    # boucle principale
+    for k in range(n):
+        # partial pivoting
+        if np.fabs(A[k, k]) < 1.0e-12:
+            for i in range(k+1, n):
+                if np.fabs(A[i, k]) > np.fabs(A[k, k]):
+                    for j in range(k, n):
+                        A[k, j], A[i, j] = A[i, j], A[k, j]
+                    B[k], B[i] = B[i], B[k]
+                    break
+        # Division of the pivot row
+        pivot = A[k, k]
+        for j in range(k, n):
+            A[k, j] /= pivot
+        B[k] /= pivot
+        # Elimination loop
+        for i in range(n):
+            if i == k or A[i, k] == 0: continue
+            factor = A[i, k]
+            for j in range(k, n):
+                A[i, j] -= factor * A[k, j]
+            B[i] -= factor * B[k]
+            
+    return B, A
+
+# ==============================================================================================
+def jaccobi():
+    pass
+# ==============================================================================================
+def decomposition_LU(A):
+    try:
+         # verifier si la matrice A est carrée
+        m, n = np.shape(A)[0], np.shape(A)[1]   # respectivemnt nb de lignes et de colonnes
+        assert m == n
+        
+        L = np.zeros(shape = (n, n))  # initilaisation de L
+        U = np.zeros(shape = (n, n))  # initilaisation de U
+        
+        for i in range(n):
+            # calcul des élément de la matrice L
+            for j in range(i-1):
+                L[i, j] = A[i, j]
+                for k in range(j-1):
+                    L[i, j] = L[i, j] - L[i, k] * U[k, j]
+                L[i, j] = L[i, j] - U[j, j]
+                
+            # calcul des élément de la matrice U
+            for j in range(i, n):
+                U[i, j] = A[i, j]
+                for k in range(i-1):
+                    U[i, j] = U[i, j] - L[i, k] * U[k, j]
+            
+        for i in range(n):
+            # rendre la diagonle de L à 1
+            L[i, i] = 1
+            
+    except AssertionError:
+        print("\nLa matrice A n'est pas carrée")
+    return L, U
+        
+# ----------------------------------------------------------------------------------------
+def calcul_Y_pour_LU(L, B):
+    n = np.size(B)  # la taille du vecteur B
+    Y = np.zeros(shape = (n, 1))
+    for i in range(n):
+        Y[i] = B[i]
+        for k in range(i-1):
+            Y[i] = Y[i] - L[i, k] * Y[k]
+    return Y
+# ----------------------------------------------------------------------------------------
+def vecteur_solution(U, Y):
+    n = np.size(Y)
+    X = np.zeros(shape = (n, 1))
+    i = n-1
+    X[i] = Y[i]
+    for k in range(i+1, n):
+        X[i] = X[i] - U[i, k] * X[k]
+        
+    X[i] = X[i] / U[i, i]
+    return X
+# ----------------------------------------------------------------------------------------
+def methode_LU(A, B):
+    L, U = decomposition_LU(A)
+    Y = calcul_Y_pour_LU(L, B)
+    X = vecteur_solution(U, Y)
+    return L, U, Y, X
+# ==============================================================================================
+
+
+# TEST
+"""A = np.array([
+    [1, 3, 1, 1],
+    [2, -1, 0, 2],
+    [5, 4, 3, -3],
+    [0, 1, 4, 2]
+])
+
+B = np.array([8, 6, -2, 0])
+print(B)"""
+
+A = np.array([
+    [1, 3, 1, 1],
+    [2, -1, 0, 2],
+    [5, 0, 3, -3],
+    [0, 1, 4, 2]
+])
+
+B = np.array([8, 6, 2, 0])
+
+# Gauss_elimination(A, B) # appel de la fonction pivot
+# X, A = Gauss_jordan(A, B)
+# X, A = Gauss_jordanV2(A, B)
+L, U, Y, X = methode_LU(A, B)
+
+
+# print("cc\n", A[0:3, 1:3])
+# for i in range(4):
+    # print(A[i][i])
+# print(np.shape(A)[0], np.shape(A)[1])
+# A, B = B, A
+# print(np.shape(A)[0])
+# print('---------')
+
+# print('================================')
+# print("L = \n\n", L)
+# print('================================')
+# print("U = \n\n", U)
+# print('================================')
+# print("Y = \n\n", Y)
+# print('================================')
+# print("X = \n\n", X)
+
+# effacer_console() # on rend au propre la console
+
+# Test des différentes méthodes
+print('\n')
+print('\n========================== E Q U A T I O N   D U  T Y P E   A * X  =  B ==========================')
+
+if 1 == 1:
+    
+    continuer = 'o' # variable servant de condition de continuation
+    while continuer == 'o':
+        
+        # affichage du menu
+        print("\n\t M E N U ")
+        
+        print("\n\t 1- ELIMINATION DE GAUSS ")
+        print("\n\t 2- GAUSS JORDAN ")
+        print("\n\t 3- LU - CROUT ")
+        print("\n\t 4- LU - CROUT ")
+        try:
+            choix = int(input("\nFaire un choix entre {1} {2} {3} {4}: "))
+            if choix in [1, 2, 3, 4]:
+                # appel aux procedures des methodes
+                if choix == 1:
+                    print("\n\t================ M E T H O D E ================")
+                    # appel fonction
+                elif choix == 2:
+                    print("\n\t================ M E T H O D E ================")
+                    # appel fonction
+                elif choix == 2:
+                    print("\n\t================ M E T H O D E ================")
+                    # appel fonction
+                elif choix == 3:
+                    print("\n\t================ M E T H O D E ================")
+                    # appel fonction
+                elif choix == 4:
+                    print("\n\t================ M E T H O D E ================")
+                    # appel fonction
+                else:
+                    pass
+            else:
+                raise PermissionError
+    
+            continuer = str.lower(input("\nVoulez vous continuer (o/n)? ")) # forcer la valeur du choix a etre en minuscule
+            if continuer != 'n' and continuer != 'o':
+                print("\n\tErreur: Saisie invalide ")
+            
+        except PermissionError:
+            print("\nChoix invalide !")
+            
+        effacer_console() # on rend au propre la console
+
+else:
+    print("\nAucune solution pour cette équation !")
+
+print('\n')
