76ff46732e2b309f5ed68f8501ad93b227329c37
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2023-01-04 01:29:18 +0000

    interpolation par la méthode de lagrange

--- interpolation.py
+++ interpolation.py
@@ -1,4 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import sin
 
-print("hello")
+def lagrange(x, y):
+    xplt = np.linspace(x[0], x[-1])
+    yplt = np.array([], float)
+    
+    for xp in xplt:
+        yp = 0
+        for xi, yi in zip(x, y):
+            yp += yi * (np.prod((xp - x[x != xi]) / (xi - x[x != xi])))
+        yplt = np.append(yplt, yp)
+        
+    # courbe
+    plt.plot(x, y, 'ro', xplt, yplt, 'b-')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+x = np.array([0, 20, 40, 60, 80, 100])
+y = np.array([26.0, 48.0, 61.6, 71.2, 74.8, 75.2], float)
+lagrange(x,y)
