bc6d8c0aea408d5d6074661e33d059024b6deab4
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2023-01-08 22:48:59 +0000

    fichier python pour les equations différentielles

--- interpolation.py
+++ interpolation.py
@@ -13,12 +13,46 @@ def lagrange(x, y):
         yplt = np.append(yplt, yp)
         
     # courbe
-    plt.plot(x, y, 'ro', xplt, yplt, 'b-')
+    plt.plot(x, y, 'ro', xplt, yplt, 'b--')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
 
+# ===============================================================================================
+def interpolation_newton(x, y, x_new):
+    
+    # xplt = np.linspace(x[0], x[-1])
+    # yplt = np.array([], float)
+    
+    # Initialiser la matrice de différences divisées
+    f = [[0 for _ in range(len(x))] for _ in range(len(x))]
+    # Remplir la première colonne de la matrice avec les valeurs de y
+    for i in range(len(x)):
+        f[i][0] = y[i]
+    # Calculer les différences divisées
+    for i in range(1, len(x)):
+        for j in range(i, len(x)):
+            f[j][i] = (f[j][i-1] - f[j-1][i-1]) / (x[j] - x[j-i])
+    # Calculer le polynôme d'interpolation de Newton
+    
+    p = 0
+    for i in range(len(x)):
+        term = f[i][i]
+        for j in range(i):
+            term *= (x_new - x[j])
+        p += term
+    print(p)
+    #     yplt = np.append(yplt, p)
+        
+    # # courbe
+    # plt.plot(x, y, 'ro', x, yplt, 'b--')
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.show()
+# =========================================================================
 
+    
 x = np.array([0, 20, 40, 60, 80, 100])
 y = np.array([26.0, 48.0, 61.6, 71.2, 74.8, 75.2], float)
-lagrange(x,y)
+# lagrange(x,y)
+interpolation_newton(x,y,3)
